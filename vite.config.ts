da360ba2f3cd7682358b3a57220520e9fb40f446
alilomolf-cloud <alilomolf@gmail.com>
2026-02-13 01:41:31 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  // This is crucial for Capacitor/Android to find assets
+  base: './', 
   build: {
     outDir: 'dist',
     emptyOutDir: true,
